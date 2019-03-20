bbe1ed734533fbfcf857afe2ece3648f8d66279a
George.Rodier <grodier3@gmail.com>
2019-03-20 16:26:51 -0400

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import App from './App';
 
 import './styles/style.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
